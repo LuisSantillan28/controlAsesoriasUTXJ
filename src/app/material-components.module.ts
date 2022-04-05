189ef61a3dc89b657620e2078c1ddebafe616c54
LuisSantillan28 <luisgmsn2001@gmail.com>
2022-04-05 17:44:01 -0500

    Inicio del proyecto

---
+++ src/app/material-components.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class MaterialComponentsModule { }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
